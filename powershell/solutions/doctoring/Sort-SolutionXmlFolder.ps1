d6260f31684ea68c676da69ad85392b6c4090ec1
Tyler Hogsett <tyhogset@microsoft.com>
2020-11-19 14:07:21 -0800

    git subrepo pull powershell
    subrepo:
      subdir:   "powershell"
      merged:   "ae94539"
    upstream:
      origin:   "https://github.com/tyler-hogsett-microsoft/cds-powershell-quickstart"
      branch:   "main"
      commit:   "ae94539"
    git-subrepo:
      version:  "0.4.2"
      origin:   "???"
      commit:   "???"

--- powershell/solutions/doctoring/Sort-SolutionXmlFolder.ps1
+++ powershell/solutions/doctoring/Sort-SolutionXmlFolder.ps1
@@ -7,6 +7,8 @@ process {
   & $PSScriptRoot\Sort-XmlFiles.ps1 `
     -Path "$Path/*.xml" `
     -TargetNodeQueries @(
-      "/ImportExportXml/SolutionManifest/MissingDependencies"
+      "/ImportExportXml/SolutionManifest/MissingDependencies",
+      "/EntityRelationships",
+      "/Entity/EntityInfo/entity/attributes"
     )
 }
---
+++ powershell/solutions/experimental/New-CrmLookup.ps1
@@ -0,0 +1,145 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$SolutionFolderPath,
+    [Parameter(Mandatory=$true)]
+    [string]$EntityLogicalName,
+    [Parameter(Mandatory=$true)]
+    [string]$AttributeDisplayName,
+    [Parameter(Mandatory=$true)]
+    [string]$TargetEntityLogicalName
+)
+
+$attributeLogicalName = "$($AttributeDisplayName.ToLower() -Replace " ", "_")_id"
+
+$xml = New-Object xml
+$xml.PreserveWhitespace = $true
+
+$entityXmlFilePath = "$SolutionFolderPath\Entities\$EntityLogicalName\Entity.xml"
+$xml.Load($entityXmlFilePath)
+$entityNode = $xml.SelectSingleNode("/Entity/EntityInfo/entity")
+$entityNode.RemoveAttribute("unmodified")
+$entityNameNode = $xml.SelectSingleNode("/Entity/Name")
+$entitySchemaName = $entityNameNode.InnerXML
+$entityDisplayName = $entityNameNode.Attributes["LocalizedName"].Value
+$relationshipSchemaName = "mdce_$($entitySchemaName)_$($attributeLogicalName)_$TargetEntityLogicalName".Substring(0, 46)
+$attributesNode = $xml.SelectSingleNode("/Entity/EntityInfo/entity/attributes")
+$attributesNode.InnerXML += 
+    $(if($attributesNode.InnerXML.Length -eq 0) { "`r`n" }) +
+"        <attribute PhysicalName=`"$attributeLogicalName`">
+          <Type>lookup</Type>
+          <Name>$attributeLogicalName</Name>
+          <LogicalName>$attributeLogicalName</LogicalName>
+          <RequiredLevel>none</RequiredLevel>
+          <DisplayMask>ValidForAdvancedFind|ValidForForm|ValidForGrid</DisplayMask>
+          <ValidForUpdateApi>1</ValidForUpdateApi>
+          <ValidForReadApi>1</ValidForReadApi>
+          <ValidForCreateApi>1</ValidForCreateApi>
+          <IsCustomField>1</IsCustomField>
+          <IsAuditEnabled>0</IsAuditEnabled>
+          <IsSecured>0</IsSecured>
+          <IntroducedVersion>1.0.0.0</IntroducedVersion>
+          <IsCustomizable>1</IsCustomizable>
+          <IsRenameable>1</IsRenameable>
+          <CanModifySearchSettings>1</CanModifySearchSettings>
+          <CanModifyRequirementLevelSettings>1</CanModifyRequirementLevelSettings>
+          <CanModifyAdditionalSettings>1</CanModifyAdditionalSettings>
+          <SourceType>0</SourceType>
+          <IsGlobalFilterEnabled>0</IsGlobalFilterEnabled>
+          <IsSortableEnabled>0</IsSortableEnabled>
+          <CanModifyGlobalFilterSettings>1</CanModifyGlobalFilterSettings>
+          <CanModifyIsSortableSettings>1</CanModifyIsSortableSettings>
+          <IsDataSourceSecret>0</IsDataSourceSecret>
+          <AutoNumberFormat></AutoNumberFormat>
+          <IsSearchable>0</IsSearchable>
+          <IsFilterable>0</IsFilterable>
+          <IsRetrievable>0</IsRetrievable>
+          <IsLocalizable>0</IsLocalizable>
+          <LookupStyle>single</LookupStyle>
+          <LookupTypes />
+          <displaynames>
+            <displayname description=`"$AttributeDisplayName`" languagecode=`"1033`" />
+          </displaynames>
+          <Descriptions>
+            <Description description=`"`" languagecode=`"1033`" />
+          </Descriptions>
+        </attribute>
+      "
+$xml.Save($entityXmlFilePath)
+
+$relationshipsXmlFilePath = "$SolutionFolderPath\Other\Relationships.xml"
+$xml.Load($relationshipsXmlFilePath)
+$entityRelationshipsNode = $xml.SelectSingleNode("/EntityRelationships")
+$entityRelationshipsNode.InnerXML +=
+    $(if($entityRelationshipsNode.InnerXML.Length -eq 0) { "`r`n" }) +
+    "  <EntityRelationship Name=`"$relationshipSchemaName`" />
+"
+$xml.Save($relationshipsXmlFilePath)
+
+$entityRelationshipsXmlFilePath = "$SolutionFolderPath\Other\Relationships\$TargetEntityLogicalName.xml"
+$xml.Load($entityRelationshipsXmlFilePath)
+$entityRelationshipsNode = $xml.SelectSingleNode("/EntityRelationships")
+$entityRelationshipsNode.InnerXML += 
+    $(if($entityRelationshipsNode.InnerXML.Length -eq 0) { "`r`n" }) +
+"  <EntityRelationship Name=`"$relationshipSchemaName`">
+    <EntityRelationshipType>OneToMany</EntityRelationshipType>
+    <IsCustomizable>1</IsCustomizable>
+    <IntroducedVersion>1.0.0.0</IntroducedVersion>
+    <IsHierarchical>0</IsHierarchical>
+    <ReferencingEntityName>$EntityLogicalName</ReferencingEntityName>
+    <ReferencedEntityName>$TargetEntityLogicalName</ReferencedEntityName>
+    <CascadeAssign>NoCascade</CascadeAssign>
+    <CascadeDelete>RemoveLink</CascadeDelete>
+    <CascadeReparent>NoCascade</CascadeReparent>
+    <CascadeShare>NoCascade</CascadeShare>
+    <CascadeUnshare>NoCascade</CascadeUnshare>
+    <CascadeRollupView>NoCascade</CascadeRollupView>
+    <IsValidForAdvancedFind>1</IsValidForAdvancedFind>
+    <ReferencingAttributeName>$attributeLogicalName</ReferencingAttributeName>
+    <RelationshipDescription>
+      <Descriptions>
+        <Description description=`"`" languagecode=`"1033`" />
+      </Descriptions>
+    </RelationshipDescription>
+    <EntityRelationshipRoles>
+      <EntityRelationshipRole>
+        <NavPaneDisplayOption>UseCollectionName</NavPaneDisplayOption>
+        <NavPaneArea>Details</NavPaneArea>
+        <NavPaneOrder>10000</NavPaneOrder>
+        <NavigationPropertyName>$attributeLogicalName</NavigationPropertyName>
+        <RelationshipRoleType>1</RelationshipRoleType>
+      </EntityRelationshipRole>
+      <EntityRelationshipRole>
+        <NavigationPropertyName>$relationshipSchemaName</NavigationPropertyName>
+        <RelationshipRoleType>0</RelationshipRoleType>
+      </EntityRelationshipRole>
+    </EntityRelationshipRoles>
+  </EntityRelationship>
+"
+$xml.Save($entityRelationshipsXmlFilePath)
+
+$targetEntityXmlPath = "$SolutionFolderPath\Entities\$TargetEntityLogicalName\Entity.xml"
+$xml.Load($targetEntityXmlPath)
+$targetEntityNameNode = $xml.SelectSingleNode("/Entity/Name")
+$targetEntityDisplayName = $targetEntityNameNode.Attributes["LocalizedName"].Value
+
+$solutionXmlFilePath = "$SolutionFolderPath\Other\Solution.xml"
+$xml.Load($solutionXmlFilePath)
+$missingDependenciesNode = $xml.SelectSingleNode("/ImportExportXml/SolutionManifest/MissingDependencies")
+$missingDependenciesNode.InnerXML += 
+    $(if($missingDependenciesNode.InnerXML.Length -eq 0) { "`r`n" }) +
+"      <MissingDependency>
+        <Required displayName=`"$entityDisplayName`" schemaName=`"$entitySchemaName`" solution=`"MicrosoftPortalBase (9.2.2006.10)`" type=`"1`" />
+        <Dependent displayName=`"$relationshipSchemaName`" parentDisplayName=`"$targetEntityDisplayName`" parentSchemaName=`"mdce_portal_module`" schemaName=`"$relationshipSchemaName`" type=`"10`" />
+      </MissingDependency>
+      <MissingDependency>
+        <Required displayName=`"$entityDisplayName`" schemaName=`"$entitySchemaName`" solution=`"MicrosoftPortalBase (9.2.2006.10)`" type=`"1`" />
+        <Dependent displayName=`"$AttributeDisplayName`" parentDisplayName=`"$entityDisplayName`" parentSchemaName=`"$entitySchemaName`" schemaName=`"$attributeLogicalName`" type=`"2`" />
+      </MissingDependency>
+      <MissingDependency>
+        <Required displayName=`"$entityDisplayName`" schemaName=`"$entitySchemaName`" solution=`"MicrosoftPortalBase (9.2.2006.10)`" type=`"1`" />
+        <Dependent parentDisplayName=`"$entityDisplayName`" parentSchemaName=`"$entitySchemaName`" schemaName=`"$($attributeLogicalName)name`" type=`"2`" />
+      </MissingDependency>
+    "
+$xml.Save($solutionXmlFilePath)
+
+& $PSScriptRoot\..\doctoring\Sort-SolutionXmlFolder.ps1 $SolutionFolderPath
