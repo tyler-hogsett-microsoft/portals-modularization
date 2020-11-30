7a438eedf72c3c81bccff37daac14f048678a13e
Tyler Hogsett <tyhogset@microsoft.com>
2020-11-30 14:42:47 -0800

    revised shortcut script to not remove subrepo

--- Export-Solution.ps1
+++ Export-Solution.ps1
@@ -1,3 +1,4 @@
 & $PSScriptRoot\powershell\solutions\Export-Solution.ps1 `
     -SolutionUniqueName mdce_portals_modularization `
     -TargetFolderPath $PSScriptRoot\solution
+git checkout -- $PSScriptRoot\solution\.gitrepo
